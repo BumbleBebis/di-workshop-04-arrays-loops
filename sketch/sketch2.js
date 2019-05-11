4fc5e87c3fa85da494798100db0760f0b37f8605
Emma Van Eeckhout <emmavaneeckhout@hotmail.com>
2019-05-11 17:03:32 +0100

    added for loops

--- sketch/sketch2.js
+++ sketch/sketch2.js
@@ -1,9 +1,5 @@
-var yOne = 20
-var yTwo = 100
-var yThree = 180
-var speedOne = 3
-var speedTwo = 3
-var speedThree = 3
+var ys = [20, 100, 180]
+var speeds = [3, 3, 3]
 
 function setup() {
   createCanvas(400, 400)
@@ -13,21 +9,11 @@ function setup() {
 function draw() {
   background(200)
 
-  yOne = yOne + speedOne
-  if (yOne < 0 || yOne > height) {
-    speedOne = speedOne * -1
+  for (var i = 0; i < 3; i++) {
+    ys[i] = ys[i] + speeds[i]
+    if (ys[i] < 0 || ys[i] > height) {
+      speeds[i] = speeds[i] * -1
+    }
+    rect(30 * (i + 1), ys[i], 20, 20)
   }
-  rect(30, yOne, 20, 20)
-
-  yTwo = yTwo + speedTwo
-  if (yTwo < 0 || yTwo > height) {
-    speedTwo = speedTwo * -1
-  }
-  rect(60, yTwo, 20, 20)
-
-  yThree = yThree + speedThree
-  if (yThree < 0 || yThree > height) {
-    speedThree = speedThree * -1
-  }
-  rect(90, yThree, 20, 20)
 }
